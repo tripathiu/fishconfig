04dc2f4095f9af70ec521a9f82e6a7ba70a90207
Utkarsh Tripathi <utkarsh.tripathi@siemens.com>
2021-07-06 12:18:33 +0530

    Update config.fish

--- config.fish
+++ config.fish
@@ -7,7 +7,7 @@ function fish_prompt -d "Write out the prompt"
 
     set -l _git (set_color grey --dim)(fish_git_prompt)(set_color normal)
     set -l _pwd (set_color green) [(set_color brgreen -i)(prompt_pwd)(set_color normal)(set_color green)](set_color normal)
-    set -l _end (set_color brred)" > "(set_color normal)
+    set -l _end (set_color brred)" ❯ "(set_color normal)
     set -l _ret (set_color white -b red) $last_status"!" (set_color normal)" "
     set -l _nop (set_color white -b green)" "(set_color normal)
 
@@ -27,11 +27,15 @@ end
 set -g fish_prompt_pwd_dir_length 2
 
 
-
-
 ###################
 #     Aliases     #
 ###################
 alias vim nvim
+alias showcase "docker run -it -v (pwd):/app docker.siemens.com/teamsae/showcase:latest"
 
+
+###################
+#     Path        #
+###################
+ contains $HOME/.local/bin $fish_user_paths; or set -Ua fish_user_paths $HOME/.local/bin
 
